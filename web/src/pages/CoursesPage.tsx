2dbeda1d76ed65c17da71aefeed633eb08d9a1f9
suzzang2 <subin030102@naver.com>
2025-01-27 23:18:03 +0900

    DESIGN: 강의 목록 test

--- web/src/pages/CoursesPage.tsx
+++ web/src/pages/CoursesPage.tsx
@@ -5,9 +5,50 @@ const CoursesPage = () => {
   return (
     <>
       <Main>
-        <Title>
-          예리 쌤과 일본어를 배워보세요!
-        </Title>
+        <Wrapper>
+          <Title>
+            예리 센세와 함께 일본어를 배워보세요!
+          </Title>
+
+          <CoursesContainer>
+            <Course>
+              <CourseImage src="/images/courseBanner/courseBanner1.png" />
+              <CourseTitle>일본어 초급</CourseTitle>
+              <CoursePrice>
+                130,000원
+              </CoursePrice>
+            </Course>
+            <Course>
+              <CourseImage src="/images/courseBanner/courseBanner1.png" />
+              <CourseTitle>일본어 초급</CourseTitle>
+              <CoursePrice>
+                130,000원
+              </CoursePrice>
+            </Course>
+            <Course>
+              <CourseImage src="/images/courseBanner/courseBanner1.png" />
+              <CourseTitle>일본어 초급</CourseTitle>
+              <CoursePrice>
+                130,000원
+              </CoursePrice>
+            </Course>
+            <Course>
+              <CourseImage src="/images/courseBanner/courseBanner1.png" />
+              <CourseTitle>일본어 초급</CourseTitle>
+              <CoursePrice>
+                130,000원
+              </CoursePrice>
+            </Course>
+            <Course>
+              <CourseImage src="/images/courseBanner/courseBanner1.png" />
+              <CourseTitle>일본어 초급</CourseTitle>
+              <CoursePrice>
+                130,000원
+              </CoursePrice>
+            </Course>
+          </CoursesContainer>
+
+        </Wrapper>
       </Main>
     </>
   )
@@ -15,8 +56,49 @@ const CoursesPage = () => {
 
 export default CoursesPage
 
-const Title = styled.div`
-  font-size: 24px;
-  font-weight: bold;
-  margin-bottom: 20px;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 90%;
 `
+
+const Title = styled.div`
+  font-size: 22px;
+  font-weight: 500;
+  margin-bottom: 40px;
+  margin-top: 40px;
+  width: 100%;
+  color: #402900;
+`
+
+const CoursesContainer = styled.div`
+  width: 100%;
+  display: flex; 
+  flex-wrap: wrap; 
+  justify-content: center;
+  gap: 70px; // 각 코스 사이의 간격
+`
+
+const Course = styled.div`
+  width: 100%;
+
+`
+
+const CourseImage = styled.img`
+  width: 100%;
+  border-radius: 10px;
+`
+
+const CourseTitle = styled.div`
+  font-size: 20px;
+  font-weight: 500;
+  margin-top: 20px;
+`
+
+const CoursePrice = styled.div`
+  font-size: 16px;
+  margin-top: 10px;
+`
+
