fea715185ebb5bab8f58d56fe4d9a149c1bc51d7
suzzang2 <subin030102@naver.com>
2025-02-06 10:53:48 +0900

    FEAT: 학생목록 페이지 +버튼 추가

--- web/src/components/adminComponents/Out_StudentsList.tsx
+++ web/src/components/adminComponents/Out_StudentsList.tsx
@@ -1,35 +1,63 @@
-import styled from "styled-components"
-import { STUDENTS_LIST } from "../../constants/studentsList" //임시로 만든 데이터 사용
-import FilterContainer from "./etc/FilterContainer"
-import { useState } from "react"
+import styled from "styled-components";
+import { STUDENTS_LIST } from "../../constants/studentsList";
+import FilterContainer from "./etc/FilterContainer";
+import { useState } from "react";
 
 const Out_StudentsList = () => {
-  const [selectedStudentId, setSelectedStudentId] = useState<number | null>(1)
+  const [selectedStudentId, setSelectedStudentId] = useState<number | null>(1);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedStudent, setEditedStudent] = useState<any>({});
 
-   // 선택한 학생의 데이터 가져오기
-  const selectedStudent = STUDENTS_LIST.find(student => student.id === selectedStudentId);
+  // 선택한 학생의 데이터 가져오기
+  const selectedStudent = STUDENTS_LIST.find(
+    (student) => student.id === selectedStudentId
+  );
 
+  // 편집 모드로 전환
+  const handleEdit = () => {
+    setIsEditing(true);
+    setEditedStudent({ ...selectedStudent });
+  };
 
+  // 입력값 변경 핸들러
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>, field: string) => {
+    setEditedStudent({ ...editedStudent, [field]: e.target.value });
+  };
+
+  // 저장 버튼 클릭 시 적용
+  const handleSave = () => {
+    if (selectedStudent) {
+      selectedStudent.name = editedStudent.name;
+      selectedStudent.birth = editedStudent.birth;
+      selectedStudent.phone = editedStudent.phone;
+      selectedStudent.class = editedStudent.class;
+    }
+    setIsEditing(false);
+  };
 
   return (
     <Wrapper>
       <StudentListContainer>
-        <Title>학생 목록</Title>
+        <Title>
+          학생 목록
+          <PlusButton>+</PlusButton>
+        </Title>
         <FilterContainer />
-        {/* 학생 목록 표 */}
         <StudentsTable>
           <TableHeader>
-            <TableHeaderItem id='name'>이름</TableHeaderItem>
-            <TableHeaderItem id='birth'>생년월일</TableHeaderItem>
-            <TableHeaderItem id='class'>수강현황</TableHeaderItem>
-
+            <TableHeaderItem>이름</TableHeaderItem>
+            <TableHeaderItem>생년월일</TableHeaderItem>
+            <TableHeaderItem>수강현황</TableHeaderItem>
           </TableHeader>
           <TableBody>
             {STUDENTS_LIST.map((student) => (
-              <TableRow 
-                key={student.id} 
-                onClick={() => setSelectedStudentId(student.id)}
-                isSelected={selectedStudentId === student.id} 
+              <TableRow
+                key={student.id}
+                onClick={() => {
+                  setSelectedStudentId(student.id);
+                  setIsEditing(false);
+                }}
+                isSelected={selectedStudentId === student.id}
               >
                 <TableItem>{student.name}</TableItem>
                 <TableItem>{student.birth}</TableItem>
@@ -41,45 +69,55 @@ const Out_StudentsList = () => {
       </StudentListContainer>
 
       <StudentDetailContainer>
-      <DetailRow>
-          <DetailTitle>사진</DetailTitle>
-          <DetailContent><StudentPhoto src="/images/studentPhotos/student_photo.jpg"/></DetailContent>
-        </DetailRow>
         <DetailRow>
           <DetailTitle>이름</DetailTitle>
-          <DetailContent>{selectedStudent?.name}</DetailContent>
+          {isEditing ? (
+            <DetailInput value={editedStudent.name || ''} onChange={(e) => handleInputChange(e, "name")} />
+          ) : (
+            <DetailContent>{selectedStudent?.name}</DetailContent>
+          )}
         </DetailRow>
         <DetailRow>
-        <DetailTitle>생년월일</DetailTitle>
-          <DetailContent>{selectedStudent?.birth}</DetailContent>
+          <DetailTitle>생년월일</DetailTitle>
+          {isEditing ? (
+            <DetailInput value={editedStudent.birth || ''} onChange={(e) => handleInputChange(e, "birth")} />
+          ) : (
+            <DetailContent>{selectedStudent?.birth}</DetailContent>
+          )}
         </DetailRow>
         <DetailRow>
           <DetailTitle>전화번호</DetailTitle>
-          <DetailContent>{selectedStudent?.phone}</DetailContent>
+          {isEditing ? (
+            <DetailInput value={editedStudent.phone || ''} onChange={(e) => handleInputChange(e, "phone")} />
+          ) : (
+            <DetailContent>{selectedStudent?.phone}</DetailContent>
+          )}
         </DetailRow>
         <DetailRow>
           <DetailTitle>수강현황</DetailTitle>
-          <DetailContent>{selectedStudent?.class}</DetailContent>
-        </DetailRow>
-        <DetailRow>
-          <DetailTitle>주소</DetailTitle>
-          <DetailContent></DetailContent>
-        </DetailRow>
-        <DetailRow>
-          <DetailTitle>특이사항</DetailTitle>
-          <DetailInput></DetailInput>
+          {isEditing ? (
+            <DetailInput value={editedStudent.class || ''} onChange={(e) => handleInputChange(e, "class")} />
+          ) : (
+            <DetailContent>{selectedStudent?.class}</DetailContent>
+          )}
         </DetailRow>
 
         <ButtonsContainer>
-          <Button>수정</Button>
-          <Button>삭제</Button>
+          {isEditing ? (
+            <Button onClick={handleSave}>저장</Button>
+          ) : (
+            <Button onClick={handleEdit}>수정</Button>
+          )}
         </ButtonsContainer>
       </StudentDetailContainer>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default Out_StudentsList
+export default Out_StudentsList;
+
+// 스타일링 코드 (생략 가능)
+
 
 const Wrapper = styled.div`
   width: 100%;
@@ -107,13 +145,29 @@ const Title = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: flex-start;
+  justify-content: space-between;
   padding-top: 20px;
   margin-bottom: 20px;
   padding-bottom: 15px;
   border-bottom: 1px solid #e1e1e1;
   font-weight: bold;
   font-size: 1.5rem;
+`
+
+const PlusButton = styled.button`
+  width: 30px;
+  height: 30px;
+  background-color: #d7d7d7;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-left: 10px;
+  font-size: 20px;
+
+  &:hover {
+    background-color: #636363;
+    color: #ffffff;
+  }
 `
 
 const StudentsTable = styled.div`
