91c61781f4e1411be62fe490b3750876c69b39d3
suzzang2 <subin030102@naver.com>
2025-02-12 22:42:41 +0900

    FEAT: 회원가입 기능 완성

--- web/src/pages/RegisterPage.tsx
+++ web/src/pages/RegisterPage.tsx
@@ -1,26 +1,82 @@
+import { useState } from "react"
 import styled from "styled-components"
 
 const RegisterPage = () => {
+   const [name, setName] = useState('')
+   const [year, setYear] = useState('')
+   const [month, setMonth] = useState('')
+   const [day, setDay] = useState('')
+   const [phone, setPhone] = useState('')
+
+   const handleNameChange = (e: any) => {
+      setName(e.target.value)
+   }
+
+   const handleYearChange = (e: any) => {
+      setYear(e.target.value)
+   }
+   const handleMonthChange = (e: any) => {
+      setMonth(e.target.value)
+   }
+   const handleDayChange = (e: any) => {
+      setDay(e.target.value)
+   }
+
+   const handlePhoneChange = (e: any) => {
+      setPhone(e.target.value)
+   }
+
+   const handleSubmit = () => {
+      //예외처리 - 이름, 생년월일, 전화번호 입력 안했을 경우
+      if(name === '' || year === '' || month === '' || day === '' || phone === '') {
+         alert('모든 항목을 입력해주세요!')
+         return
+      }
+
+      //예외처리 - 생년월일 숫자가 아닌 경우
+      if(isNaN(Number(year)) || isNaN(Number(month)) || isNaN(Number(day))) {
+         alert('생년월일은 숫자로 입력해주세요!')
+         return
+      }
+
+      //예외처리 - 전화번호 숫자가 아닌 경우
+      if(isNaN(Number(phone))) {
+         alert('전화번호는 숫자만 입력해주세요!')
+         return
+      }
+
+      //예외처리 - 생년월일 각 길이가 맞지 않는 경우
+      if(year.length !== 4 || month.length !== 2 || day.length !== 2) {
+         alert('생년월일을 형식에 맞게 정확히 입력해주세요!')
+         return
+      }
+
+      //등록
+      alert('등록되었습니다!');
+      console.log(name, year, month, day, phone);
+   }
+
+
    return (
       <Wrapper>
          <Title>회원 정보 등록</Title>
          <InputRow>
             <InputTitle>이름</InputTitle>
-            <Input placeholder="이름을 입력해주세요" />
+            <Input placeholder="이름을 입력해주세요" onChange={handleNameChange} value={name}/>
          </InputRow>
          <InputRow style={{flexDirection:'column', justifyContent:'space-between'}}>
             <InputTitle>생년월일</InputTitle>
             <Birth>
-               <BirthInput id="year" placeholder="yyyy"/>
-               <BirthInput id="month" placeholder="mm"/>
-               <BirthInput id="day" placeholder="dd"/>
+               <BirthInput id="year" placeholder="yyyy" onChange={handleYearChange} value={year} maxLength={4}/>
+               <BirthInput id="month" placeholder="mm" onChange={handleMonthChange} value={month} maxLength={2}/>
+               <BirthInput id="day" placeholder="dd" onChange={handleDayChange} value={day} maxLength={2}/>
             </Birth>
          </InputRow>
          <InputRow>
             <InputTitle>전화번호</InputTitle>
-            <Input placeholder="비밀번호를 입력해주세요" />
+            <Input placeholder="01012345678" onChange={handlePhoneChange} value={phone}/>
          </InputRow>
-         <SubmitBtn>등록</SubmitBtn>
+         <SubmitBtn onClick={handleSubmit}>등록</SubmitBtn>
       </Wrapper>
    )
 }
@@ -38,7 +94,7 @@ const Wrapper = styled.div`
 const Title = styled.div`
    font-size: 20px;
    font-weight: 'bold';
-   margin-top: 30px;
+   margin-top: 90px;
 `
 
 const InputRow = styled.div`
@@ -52,8 +108,7 @@ const InputRow = styled.div`
 
 const InputTitle = styled.div`
    width: 100%;
-   font-size: 13px;
-   margin-right: 10px;
+   font-size: 13px; 
    text-align: left;
    margin-bottom: 5px;
    color: #333;
@@ -97,6 +152,7 @@ const SubmitBtn = styled.button`
    border-radius: 10px;
    cursor: pointer;
    font-size: 15px;
+   margin-bottom: 90px;
 
    &:hover {
       background-color: #392a20;
