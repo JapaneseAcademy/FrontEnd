f2574f6ff9deddabe12d08ba854b66fe59edcd67
suzzang2 <subin030102@naver.com>
2025-03-02 23:24:55 +0900

    FEAT: 강의목록 하드코딩

--- web/src/pages/CoursesPage.tsx
+++ web/src/pages/CoursesPage.tsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 import Main from "../components/Main";
 import styled from "styled-components";
 import Course from "../components/Course";
-import { getCourses } from "../apis/courseAPI";
 import { convertTags } from "../utils/utils";
+import { realCourses } from "../constants/real/realCourses";
 
 type course = {
   courseId: string;
@@ -13,13 +13,12 @@ type course = {
   courseCost: number;
 }
 
+//임시 하드코딩용
 const CoursesPage = () => {
   const [courses, setCourses] = useState<course[]>([]);
 
   useEffect(() => {
-    getCourses().then((data) => {
-      const courseInfos = data.courseInfos;
-      const formattedCourses = courseInfos.map((course: any) => ({
+      const formattedCourses = realCourses.map((course: any) => ({
         courseId: course.courseId,
         courseImage: course.mainImageUrl,
         tags: convertTags(course.isLive, course.isOnline, course.isRecorded),
@@ -28,8 +27,8 @@ const CoursesPage = () => {
       }));
   
       setCourses(formattedCourses); // ✅ 한 번에 전체 데이터 세팅
-    });
-  }, []);
+  }
+  , []);
   
 
   useEffect(() => {
@@ -90,8 +89,88 @@ const CoursesPage = () => {
     </>
   );
 };
-
 export default CoursesPage;
+
+// 나중에 api 완성되면 이걸로 다시 작업
+// const CoursesPage = () => {
+//   const [courses, setCourses] = useState<course[]>([]);
+
+//   useEffect(() => {
+//     getCourses().then((data) => {
+//       const courseInfos = data.courseInfos;
+//       const formattedCourses = courseInfos.map((course: any) => ({
+//         courseId: course.courseId,
+//         courseImage: course.mainImageUrl,
+//         tags: convertTags(course.isLive, course.isOnline, course.isRecorded),
+//         courseTitle: course.title,
+//         courseCost: course.cost
+//       }));
+  
+//       setCourses(formattedCourses); // ✅ 한 번에 전체 데이터 세팅
+//     });
+//   }, []);
+  
+
+//   useEffect(() => {
+//     console.log("세팅 후:", courses);
+//   }
+//   , [courses]);
+
+//   const [currentPage, setCurrentPage] = useState(1); // 현재 페이지 상태
+//   const coursesPerPage = 3; // 페이지당 표시할 코스 개수
+
+//   // 현재 페이지에 표시할 코스 계산
+//   const startIndex = (currentPage - 1) * coursesPerPage;
+//   const endIndex = startIndex + coursesPerPage;
+//   const currentCourses = courses.slice(startIndex, endIndex);
+
+//   // 페이지 변경 핸들러
+//   const handlePageChange = (pageNumber: number) => {
+//     setCurrentPage(pageNumber);
+//     // window.scrollTo({ top: 0, behavior: 'smooth' }); // 페이지 최상단으로 스크롤, 부드럽게 이동
+//     window.scrollTo(0, 0);
+//   };
+
+//   // 전체 페이지 수 계산
+//   const totalPages = Math.ceil(courses.length / coursesPerPage);
+//   return (
+//     <>
+//       <Main>
+//         <Wrapper>
+//           <Title>예리 센세와 함께 일본어를 배워보세요!</Title>
+
+//           <CoursesContainer>
+//             {currentCourses.map((course: course) => (
+//               <Course
+//                 key={course.courseId}
+//                 courseId={course.courseId}
+//                 courseImage={course.courseImage}
+//                 courseTitle={course.courseTitle}
+//                 courseCost={course.courseCost}
+//                 Tags={course.tags}
+//               />
+//             ))}
+//           </CoursesContainer>
+
+//           {/* 페이지네이션 */}
+//           <Pagination>
+//             {Array.from({ length: totalPages }, (_, index) => (
+//               <PageButton
+//                 key={index + 1}
+//                 isActive={currentPage === index + 1}
+//                 onClick={() => handlePageChange(index + 1)}
+//               >
+//                 {index + 1}
+//               </PageButton>
+//             ))}
+//           </Pagination>
+//         </Wrapper>
+//       </Main>
+//     </>
+//   );
+// };
+
+// export default CoursesPage;
 
 const Wrapper = styled.div`
   display: flex;
