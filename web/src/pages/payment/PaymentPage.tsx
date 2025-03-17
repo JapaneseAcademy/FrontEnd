c332402a26d0ba9a0b9683183f141e53e51ba5e2
suzzang2 <subin030102@naver.com>
2025-03-18 01:14:09 +0900

    FEAT: 클라이언트 키 다시 test로 변경

--- web/src/pages/payment/PaymentPage.tsx
+++ web/src/pages/payment/PaymentPage.tsx
@@ -41,7 +41,7 @@ const PaymentPage = () => {
          value: coursePrice,
       });
       async function fetchPaymentWidgets() {
-         const tossPayments = await loadTossPayments(import.meta.env.VITE_TOSSPAYMENTS_CLIENT_KEY_LIVE);
+         const tossPayments = await loadTossPayments(import.meta.env.VITE_TOSSPAYMENTS_CLIENT_KEY_TEST);
          const widgets = tossPayments.widgets({ customerKey });
          setWidgets(widgets);
       }
