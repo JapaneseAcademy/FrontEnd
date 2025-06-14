c23e562c4cf3d2c59c6c1a2d66d937c92c6ea815
suzzang2 <subin030102@naver.com>
2025-06-14 21:14:08 +0900

    DOCS: CoursesPage 상단 문구 변경

--- web/src/pages/CoursesPage.tsx
+++ web/src/pages/CoursesPage.tsx
@@ -91,7 +91,7 @@ const CoursesPage = () => {
           { isLoading ? (<Loading />)
           : (
           <>
-            <Title>예리 센세와 함께 일본어를 배워보세요!</Title>
+            <Title>국내파 고수들과 함께 일본어 세계로 넘어와보세요!</Title>
             <LevelDropdown onChange={handleLevelChange}>
               <option value="all">전체</option>
               <option value="기초">기초</option>
